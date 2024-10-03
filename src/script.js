e19cbaf1eeda00772e53360db859e7629f3a5e26
Ярослав Скорынин <yaroslav.skorynin@mail.ru>
2024-10-03 22:14:48 +0300

    Создан репозиторий

---
+++ src/script.js
@@ -0,0 +1,30 @@
+// fetch("https://fakestoreapi.com/products")
+//     .then((res) => res.json())
+//     .then((json) => console.log(json));
+const API_URL = 'https://fakestoreapi.com'
+
+const imagesPlace = document.querySelector('article');
+
+async function getProducts() {
+    try {
+        const response = await fetch(`${API_URL}/products`)
+        if(!response.ok) throw new Error('Network response was not ok');
+        const products = await response.json();
+        console.log(products)
+        setImages(products);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+function setImages(products) {
+    const images = products.map(index => index.image);
+    const innerHTML = images.reduce((acc, item) => {
+        const innerHTML = acc + `
+        <img src="${item}">`
+        return innerHTML;
+    }, '')
+    imagesPlace.innerHTML = innerHTML;
+}
+
+getProducts();
